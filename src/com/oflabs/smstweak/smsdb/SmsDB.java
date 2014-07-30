3077b6905aae66a8088bf795b97c989284759061
Olivier Fauchon <olivier@oflabs.com>
2014-07-30 19:08:23 +0200

    Copyrights & license

--- src/com/oflabs/smstweak/smsdb/SmsDB.java
+++ src/com/oflabs/smstweak/smsdb/SmsDB.java
@@ -1,3 +1,20 @@
+/*
+Copyright (C) 2013, 2014 Olivier Fauchon
+This file is part of SMS-TWEAK Android Aplication.
+
+SMS-TWEAK is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SMS-TWEAK is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+*/
+
 package com.oflabs.smstweak.smsdb;
 
 import android.content.ContentValues;
